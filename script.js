544cb7d0a5f6ab23d272fd5a4d4ef5bd9fa11319
GadWitczak <119819421+GadWitczak@users.noreply.github.com>
2023-07-04 17:52:58 -0300

    Questões 3 e 4 feitas

--- script.js
+++ script.js
@@ -29,3 +29,59 @@ function listImages(url) {
       console.error(error);
     });
 /////////////////////////////////////////////////////////////////////    
+
+function mostraImagem(imagem){
+  let listarimagensDir = document.getElementById("apresentarimagenscoluna1");
+  let listarimagensEsq = document.getElementById("apresentarimagenscoluna2");
+
+  let card = document.createElement('div');
+  card.className = 'card';
+
+  let thumbnail = document.createElement('img');
+  thumbnail.textContent = "teste";
+  thumbnail.src = imagem.thumbnailUrl;
+  thumbnail.alt = imagem.title;
+  let title = document.createElement('h4');
+  title.textContent = imagem.title;
+  thumbnail.onclick = () => abrelightbox(imagem.url, imagem.title);
+
+  card.appendChild(title);
+  card.appendChild(thumbnail);
+
+
+  if (listarimagensEsq.childElementCount <= listarimagensDir.childElementCount) {
+    listarimagensEsq.appendChild(card);
+  } else {
+    listarimagensDir.appendChild(card);
+  }
+}
+
+function abrelightbox(imageurl, titulo){
+  let lightbox = document.getElementById('lightbox');
+  let lightboxImage = document.getElementById('lightboxImage');
+  let h3dalightbox = document.getElementById('h3dalightbox');
+
+  lightbox.style.display = 'block';
+  h3dalightbox.innerText = titulo;
+  lightboxImage.src = imageurl;
+  lightboxImage.alt = titulo;
+}
+
+function fechaLightbox() {
+  let lightbox = document.getElementById('lightbox');
+  lightbox.style.display = 'none';
+}
+
+listImages('https://jsonplaceholder.typicode.com/photos')
+  .then(imagens => {
+    if (imagens) {
+      imagens.forEach(imagem => {
+        mostraImagem(imagem);
+      });
+    } else {
+      console.log('Nenhum objeto recebido');
+    }
+  })
+  .catch(error => {
+    console.error(error);
+  });
